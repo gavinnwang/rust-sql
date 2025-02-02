8761f675c7737359af5a1b687f940471b49019ef
Gavin <79772347+gavinnwang@users.noreply.github.com>
2025-02-02 16:02:18 -0600

    get tuple

--- crates/storage/src/heap/table_heap.rs
+++ crates/storage/src/heap/table_heap.rs
@@ -1,6 +1,15 @@
 use std::sync::{Arc, RwLock};
 
-use crate::{buffer_pool::BufferPoolManager, typedef::PageId};
+use rustdb_error::{errdata, Error};
+
+use crate::{
+    buffer_pool::BufferPoolManager,
+    page::table_page::{TablePageRef, TupleMetadata},
+    record_id::RecordId,
+    tuple::Tuple,
+    typedef::PageId,
+    Result,
+};
 
 pub struct TableHeap {
     page_cnt: u32,
@@ -10,8 +19,7 @@ pub struct TableHeap {
 }
 
 impl TableHeap {
-    pub fn new(bpm: &Arc<RwLock<BufferPoolManager>>) -> TableHeap {
-        let bpm = Arc::clone(bpm);
+    pub fn new(bpm: Arc<RwLock<BufferPoolManager>>) -> TableHeap {
         let first_page_id = {
             let mut bpm_handle = bpm.write().unwrap();
             let mut root_page_handle = bpm_handle
@@ -26,5 +34,16 @@ impl TableHeap {
             first_page_id,
             last_page_id: first_page_id,
         }
+    }
+
+    pub fn get_tuple(&self, rid: &RecordId) -> Result<(TupleMetadata, Tuple)> {
+        let mut bpm = self.buffer_pool_manager.write().unwrap();
+        let page_handle = bpm
+            .fetch_page_handle(rid.page_id())
+            .ok_or(Error::IO(rid.to_string()))?;
+
+        let table_page_ref = TablePageRef::from(page_handle);
+
+        table_page_ref.get_tuple(rid)
     }
 }
